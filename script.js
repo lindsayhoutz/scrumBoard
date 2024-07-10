117e003b8d9c89f88a84e54989bd87a12b9d91de
Tavin Sowell <tavinsowell@gmail.com>
2024-07-10 13:34:26 -0600

    clean up

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var del = document.getElementsByClassName("delete")
+var del = document.getElementById("delete")
 var sbmtBtn = document.getElementById("sbmt")
 var stckytxt = document.getElementsByClassName("stickytext")
 var notes =  document.getElementsByClassName("note-container")
 const noteTxt = document.querySelector('#stickytext')
-
+const allNotes = []
 btn.onclick = function() {
   modal.style.display = "block";
 }
@@ -25,10 +25,16 @@ sbmtBtn.addEventListener("click", addElement);
 
 
 function addElement() {
-
+  modal.style.display = "none";
 const para = document.createElement("p");
 const node = document.createTextNode(noteTxt.value);
 para.appendChild(node);
 const element = document.getElementById("note-container");
 element.appendChild(para);
+allNotes.push(noteTxt.value);
+console.log(allNotes)
+}
+del.addEventListener("click", removeElement);
+function removeElement(){
+delete allNotes[0]
 }
