32a774f222de202aace0956a2fa3267a0293eb8d
Tavin Sowell <tavinsowell@gmail.com>
2024-07-10 20:28:50 -0600

    delete button

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-var del = document.getElementById("delete")
+var delmodal = document.getElementById("delModal")
+var del = document.getElementById("del")
 var sbmtBtn = document.getElementById("sbmt")
-var stckytxt = document.getElementsByClassName("stickytext")
+var deltxt = document.getElementById("delTxt")
+var resetBtn = document.getElementById("Reset")
 var notes =  document.getElementsByClassName("noteContainer")
 const noteTxt = document.querySelector('#stickytext')
-const allNotes = []
+const allNotes = ["placeholder",]
+var crtTxt = document.getElementById("createTxt")
 btn.onclick = function() {
   modal.style.display = "block";
+  delmodal.style.display = "none";
+  sbmtBtn.style.display = "block";
+  crtTxt.style.display = "block";
+  deltxt.style.display = "none";
 }
 span.onclick = function() {
   modal.style.display = "none";
+ 
 }
-
-
+delmodal.onclick = function(){
+  modal.style.display = "none";
+  const htmlElement = document.getElementById(noteTxt.value);
+  htmlElement.remove();
+}
+del.onclick = function(){
+  modal.style.display = "block";
+  sbmtBtn.style.display = "none";
+  delmodal.style.display = "block";
+  crtTxt.style.display = "none";
+  deltxt.style.display = "block";
+}
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -22,9 +40,9 @@ window.onclick = function(event) {
     
   }
 }
-
+console.log(allNotes.length)
 sbmtBtn.addEventListener("click", addElement);
- num = allNotes.lenght;
+
 
 function addElement() {
   modal.style.display = "none";
@@ -36,7 +54,7 @@ para.setAttribute("class", "box note border");
 para.setAttribute("ondragstart", "drag(event)");
 para.setAttribute("draggable", "true");
 para.setAttribute("id", allNotes.length)
-const node = document.createTextNode(num + noteTxt.value);
+const node = document.createTextNode(allNotes.length + ". " + noteTxt.value);
 para.appendChild(node);
 const element = document.getElementById("noteContainer");
 element.appendChild(para);
@@ -45,7 +63,7 @@ console.log(allNotes)
 }
 del.addEventListener("click", removeElement);
 function removeElement(){
-delete allNotes[0]
+
 }
 
 
@@ -62,3 +80,6 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
+resetBtn.onclick = function(){
+  localStorage.clear()
+}
